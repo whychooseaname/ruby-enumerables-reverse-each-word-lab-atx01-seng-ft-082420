45f924a1f34af884d52fdb5b0b3543a6ba812059
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-09 03:01:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
 require 'pry'
 
 def reverse_each_word(array)
- array = array.split.collect 
-    array.reverse do |reverse|
-      reverse.join
+ array.split.collect do |reverse|
+      reverse.reverse.join
   end
+end
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
